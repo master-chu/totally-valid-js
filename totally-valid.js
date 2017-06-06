1da87e01ac9a7072ab9eee0e3bcbfd399e2b1af8
master-chu <master-chu@users.noreply.github.com>
2017-06-06 15:47:40 -0400

    create the file

---
+++ totally-valid.js
@@ -0,0 +1,24 @@
+// F
+(true&&!!![[[![[![!![]]]]]]]).toString()[0.0]
+
+// U
+JSON.stringify(parseInt(typeof typeof typeof typeof typeof _--, 9876543.997372873788723 - 323)).charAt(1&1&1&1&1&1)
+
+// C
+Object.getPrototypeOf({}).toLocaleString().split("").filter(function($_$_$_) { return $_$_$_ > "b" && $_$_$_ < "d"})[false||9-9]
+
+// K
+"k"
+
+
+// Y
+({ false: function(){return "x"}(), null: () => "y", undefined:"z" }[({undefined: null})[undefined]])()
+
+// O
+window.getComputedStyle(document.createElement("oOoOoOoOoOo"))["align-content"].slice(1,2)
+
+// U
+"u"
+
+// The whole thing:
+[(true&&!!![[[![[![!![]]]]]]]).toString()[0.0],JSON.stringify(parseInt(typeof typeof typeof typeof typeof _--, 9876543.997372873788723 - 323)).charAt(1&1&1&1&1&1),Object.getPrototypeOf({}).toLocaleString().split("").filter(function($_$_$_) { return $_$_$_ > "b" && $_$_$_ < "d"})[false||9-9],"k ",({ false: function(){return "x"}(), null: () => "y", undefined:"z" }[({undefined: null})[undefined]])(),window.getComputedStyle(document.createElement("oOoOoOoOoOo"))["align-content"].slice(1,2),"u"].reduce((xx, x)=> xx.concat(x), "")
