15eed79335599622e4114c580c91b97fbb82d210
master-chu <master-chu@users.noreply.github.com>
2017-08-24 10:54:17 -0400

    plus plus plus

--- totally-valid.js
+++ totally-valid.js
@@ -22,3 +22,7 @@ window.getComputedStyle(document.createElement("oOoOoOoOoOo"))["align-content"].
 
 // The whole thing:
 [(true&&!!![[[![[![!![]]]]]]]).toString()[0.0],JSON.stringify(parseInt(typeof typeof typeof typeof typeof _--, 9876543.997372873788723 - 323)).charAt(1&1&1&1&1&1),Object.getPrototypeOf({}).toLocaleString().split("").filter(function($_$_$_) { return $_$_$_ > "b" && $_$_$_ < "d"})[false||9-9],"k ",({ false: function(){return "x"}(), null: () => "y", undefined:"z" }[({undefined: null})[undefined]])(),window.getComputedStyle(document.createElement("oOoOoOoOoOo"))["align-content"].slice(1,2),"u"].reduce((xx, x)=> xx.concat(x), "")
+
+
+// Also do this:
++"223" + +"456"
